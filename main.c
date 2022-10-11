765593f5b97ddbe112107e66891df97e9a215063
신지윤 <sin020511@gmail.com>
2022-10-11 16:43:28 +0900

    2-1

--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+void func2(void)
+{
+	int x;
+	printf("func2 x is at %p \n", &x);
+}
+
 void func(void)
 {
 	int x;
 	
 	printf("func x is at %p \n", &x);
+	
+	func2();
 }
 
 int main(int argc, char *argv[])
